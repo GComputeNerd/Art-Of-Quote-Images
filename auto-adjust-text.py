758c6240957eec0bd79f4feca95fcfd7dff6e325
GComputeNerd <GComputeNerd@users.noreply.github.com>
2020-12-29 23:01:55 +0530

    Fixed an issue but now the Sun Tzu, The Art of War text is misaligned.

--- auto-adjust-text.py
+++ auto-adjust-text.py
@@ -19,8 +19,7 @@ cr.set_source_rgb(255,255,255)
 pc = PangoCairo.create_context(cr)
 layout = PangoCairo.create_layout(cr)
 
-#text = input("Text = ")
-text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in ligula vulputate, fermentum arcu at, viverra augue. Ut et posuere tellus. Donec varius elit orci, id bibendum nisi sollicitudin sollicitudin. Aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel blandit magna. Donec nec felis id neque faucibus fringilla. Morbi ornare est id condimentum feugiat."
+text = "Let your plans be as dark and as impenetrable as night, and when you move, fall like a thunderbolt"
 text = "Hi"
 
 font = "monospace"
@@ -31,9 +30,17 @@ layout.set_font_description(Pango.FontDescription(font + " " + style + " "  + st
 
 layout.set_width(mw)
 
+layout.set_alignment(1)
+
 layout.set_text(text)
 
 height = layout.get_extents()[1].height/Pango.SCALE
+width = layout.get_extents()[1].width
+
+while (width > mw):
+    fs -= 5
+    layout.set_font_description(Pango.FontDescription(font + " " + style + " "  + str(fs)))
+    width = layout.get_extents()[1].width
 
 while (height > mh):
     fs -= 5
@@ -41,12 +48,17 @@ while (height > mh):
     height = layout.get_extents()[1].height/Pango.SCALE
 
 width = layout.get_extents()[1].width/Pango.SCALE
+height = layout.get_extents()[1].height/Pango.SCALE
+
+x = (980-width)/2
+y = (387-height)/2
 
 cr.move_to((980-width)/2 , (387-height)/2)
 
 PangoCairo.show_layout(cr, layout)
 
-# cf.Polygon(cr,260,40,720,40,720,347,260,347)
+#cf.Polygon(cr,260,40,720,40,720,347,260,347)
+#cf.Polygon(cr,x,y,x+width,y,x+width,y+height,x,y+height)
 
 layout.set_font_description(Pango.FontDescription("monospace normal 18"))
 
@@ -54,7 +66,12 @@ layout.set_text("~ Sun Tzu, The Art of War")
 
 w = layout.get_extents()[1].width/Pango.SCALE
 
-cr.move_to(980+width-2*w, (387+height)/2)
+print(width,w,width-w)
+print(fs)
+if (width - w < 0):
+    cr.move_to(720-w, (387+height)/2)
+else:
+    cr.move_to((980+width-2*w)/2,(387+height)/2)
 
 PangoCairo.show_layout(cr, layout)
 
